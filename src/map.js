3827a79a93da02b4de8738dc637ed1ca104ae797
szpx <sesdi@protonmail.com>
2020-07-18 16:27:25 +0200

    Feature : Add zoom and resetmap view
    When you click on a marker, you zoom-in on the location and switch to
    satellite mode.
    A button is created to return to the initial state of the map.

--- src/map.js
+++ src/map.js
@@ -16,6 +16,29 @@ function addMarkerOnMap(dream) {
     map: map
   });
 
-}
+  marker.addListener('click', function() {
+    zoomOn(marker.getPosition());
+  });
+
+  addMapListeners();
+  }
+
+  const resetMapButton = document.getElementById("reset-map");
+
+  function addMapListeners() {
+    resetMapButton.addEventListener("click", resetMap); 
+  }
+
+  function zoomOn(position) {
+    map.setZoom(20);
+    map.setCenter(position);
+    map.setMapTypeId("satellite")
+  }
+
+  function resetMap() {
+    map.setCenter({ lat: 0 , lng: 0 });
+    map.setMapTypeId("roadmap");
+    map.setZoom(2);
+  }
 
 export {initMap,addMarkerOnMap};
